22d07a7c0329d92ee955f9b51eae288bd8cde8c9
Noah Rajko <wif507@haw-hamburg.de>
2025-10-23 15:09:28 +0200

    stack header added

---
+++ Programs/GSP01_Taneschnerchner/Inc/stack.h
@@ -0,0 +1,41 @@
+/**
+  * @file stack.h
+  * @author Noah Rajko
+  * @date Oct 2025
+  * @brief Header file of stack module
+  */
+
+#ifndef STACK_H
+#define STACK_H
+
+// Größe des Stacks = 8 
+
+// Abspeicherung im Stack
+
+// get from Stack (1)
+
+// get from Stack (2) 
+
+// Swap Command 
+
+// duplicate Command
+
+ //!< Token for print command 
+   //!< Token for swap command 
+  //!< Token for print total stack command
+ //!< Token for clear command
+//!< Token for double top of stack command 
+ //!< Token for ENTER button
+/*
+ ****************************************************************************************
+ *  @brief      This function prints a string to stdout terminal.
+ *
+ *  @param      msg The value that will be printed on on the terminal.
+ *
+ *  @return     void 
+ ****************************************************************************************/
+extern void printStdout(char * msg);
+
+#endif
+
+// EOF
