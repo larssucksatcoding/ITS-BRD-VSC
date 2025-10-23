99a4baebddac51af40f4451bea86cb90af72d41f
larssucksatcoding <wlz382@haw-hamburg.de>
2025-10-23 16:10:05 +0200

    add .h and .c files via cmsis to project

--- Programs/GSP01_Taneschnerchner/tmp/GSP01_Taneschnerchner.Debug+ITSboard/groups.cmake
+++ Programs/GSP01_Taneschnerchner/tmp/GSP01_Taneschnerchner.Debug+ITSboard/groups.cmake
@@ -209,6 +209,8 @@ add_library(Group_Program_User_Src OBJECT
   "${SOLUTION_ROOT}/Src/display.c"
   "${SOLUTION_ROOT}/Src/scanner.c"
   "${SOLUTION_ROOT}/Src/stack.c"
+  "${SOLUTION_ROOT}/Src/errno.c"
+  "${SOLUTION_ROOT}/Src/operator.c"
 )
 target_include_directories(Group_Program_User_Src PUBLIC
   $<TARGET_PROPERTY:${CONTEXT},INTERFACE_INCLUDE_DIRECTORIES>
