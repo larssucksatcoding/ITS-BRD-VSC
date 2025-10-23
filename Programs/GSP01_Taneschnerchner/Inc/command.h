bb99b5767932a5484888d4f41443b23ebc0795e6
larssucksatcoding <wlz382@haw-hamburg.de>
2025-10-23 16:00:37 +0200

    create command.h

---
+++ Programs/GSP01_Taneschnerchner/Inc/command.h
@@ -0,0 +1,49 @@
+
+#ifndef COMMAND_H
+#define COMMAND_H
+
+/*
+Die Funktion p druckt den obersten Wert des Stacks aus, ohne dass der Stack verändert wird.
+Ø Die Funktion P druckt den gesamten Stack aus, ohne dass der Stack verändert wird.
+Ø Die Funktion C löscht alle Einträge des Stacks.
+Ø Die Funktion d dupliziert den obersten Eintrag des Stacks.
+Ø Die Funktion r vertauscht die Reihenfolge der beiden obersten Einträge des Stacks.
+*/
+
+
+/*
+ **************************************************************
+ * @brief: prints the value of the topmost element on the stack
+ *************************************************************/
+void printTopOfStack();
+
+/*
+ **************************************************************
+ * @brief: prints the entire stack
+ *************************************************************/
+void printStack();
+
+/*
+ **************************************************************
+ * @brief: deletes all elements from the stack
+ *************************************************************/
+void deleteStack();
+
+/*
+ **************************************************************
+ * @brief: copies the topmost element of the stack and pushes
+ * that to the stack.
+ * @return: returns STACK_UNDERFLOW when the stack is empty
+ *************************************************************/
+int duplicateTopOfStack();
+
+/*
+ **************************************************************
+ * @brief: reverses the order of the two topmost elements of
+ * the stack
+ * @return: returns STACK_UNDERFLOW when the stack has fewer
+ * than two elements.
+ *************************************************************/
+int swapFristTwoEntries();
+
+#endif
