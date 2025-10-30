a7881ecc5afb30500b66541501e65ad289dec3b1
Noah Rajko <wif507@haw-hamburg.de>
2025-10-30 10:02:03 +0100

    Zahlen negativ gemacht add neg_inp

--- Programs/GSP01_Taneschnerchner/Inc/errno.h
+++ Programs/GSP01_Taneschnerchner/Inc/errno.h
@@ -9,9 +9,10 @@
 #define ERRNO_h
 
 #define NO_ERROR            0       // kein Fehler
-#define DIVISION_BY_ZERO    1       // Bei Division: teiler == 0
-#define ARITHMETIC_OVERFLOW 2       // Bereichsüberschreitung (Add, Mul, Sub)
-#define STACK_OVERFLOW      3       // Stack voll
-#define STACK_UNDERFLOW     4       // Stack leer
+#define DIVISION_BY_ZERO    -1      // Bei Division: teiler == 0
+#define ARITHMETIC_OVERFLOW -2      // Bereichsüberschreitung (Add, Mul, Sub)
+#define STACK_OVERFLOW      -3      // Stack voll
+#define STACK_UNDERFLOW     -4      // Stack leer
+#define NEGATIVE_INPUT      -5      // negative Zahl wurde eingegeben
 
 #endif
