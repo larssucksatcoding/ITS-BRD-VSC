17a2817aed853c971a6e5aaa81d45d9793bc7e5b
Noah Rajko <wif507@haw-hamburg.de>
2025-11-06 12:08:37 +0100

    error beautification

--- Programs/GSP01_Taneschnerchner/Src/errno.c
+++ Programs/GSP01_Taneschnerchner/Src/errno.c
@@ -17,32 +17,32 @@ void errorPrint (int error) {
 	setErrMode();
 	switch(error) {
 		case DIVISION_BY_ZERO: {
-			printStdout("nicht durch 0 teilen du Idiot!\n"
-				"error: DIVSION BY ZERO");
+			printStdout(" nicht durch 0 teilen\n Grundschulschwaenzer!"
+				"\n    error:\n  DIVSION BY ZERO");
 			break;
 		}
 		case ARITHMETIC_OVERFLOW: {
-			printStdout("so weit kann ich nicht Zählen.\n"
-				"error: ARITHMETIC OVERFLOW");
+			printStdout(" so weit kann ich\n nicht zaehlen.\n"
+				"\n    error:\n  ARITHMETIC OVERFLOW");
 			break;
 		}
 		case STACK_OVERFLOW: {
-			printStdout("Ich bin voll, ich kann nicht mehr.\n"
-				"error: STACK OVERFLOW");
+			printStdout(" Ich bin voll,\n ich kann nicht mehr.\n"
+				"\n    error:\n  STACK OVERFLOW");
 			break;
 		}
 		case STACK_UNDERFLOW: {
-			printStdout("Was willst du? ich kann dir nichts geben.\n"
-				"error: STACK UNDERFLOW");
+			printStdout(" Was willst du?\n Ich bin leer.\n"
+				"\n    error:\n  STACK UNDERFLOW");
 			break;
 		}
 		case NEGATIVE_INPUT: {
-			printStdout("5 Äpfel kannst du mir geben, aber -5?!\n"
-				"error: NEGATIVE INPUT");
+			printStdout(" 5 aepfel kannst du mir geben, aber -5?!\n"
+				"\n    error:\n  NEGATIVE INPUT");
 			break;
 		}
-		printStdout("\n\n\n  press C to reset");
-	} 
+	}
+	printStdout("\n\n\n  press C to reset");
 }
 
 
