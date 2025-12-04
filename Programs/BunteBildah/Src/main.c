36b4c7af101231727482745235433f5d0f9acf8f
larssucksatcoding <wlz382@haw-hamburg.de>
2025-12-04 11:04:05 +0100

    use noahs function instead

--- Programs/BunteBildah/Src/main.c
+++ Programs/BunteBildah/Src/main.c
@@ -27,26 +27,14 @@
 
 
 int main(void) {
+
+	// init
 	initITSboard();    // Initialisierung des ITS Boards
-	
 	GUI_init(DEFAULT_BRIGHTNESS);   // Initialisierung des LCD Boards mit Touch
-	TP_Init(false);                 // Initialisierung des LCD Boards mit Touch
-
-  // Begruessungstext	
-	lcdPrintlnS("Hallo liebes TI-Labor (c-project)");
-	
-	// Test in Endlosschleife
-	while(1) {
-		HAL_Delay(10000);
-	}
-	
-
-	// teilaufgabe 1
-
 	initInput();
 
 	while (true) {
-		openNextFile();
+		load_picture();
 		draw_picture();
 
 		wait_until_button_pressed();
