918510c6dc0a9ebbcc6f69e0b22188ee19a1ec2d
larssucksatcoding <wlz382@haw-hamburg.de>
2025-12-10 16:41:53 +0100

    simplify color module & add comments to header file

--- Programs/BunteBildah/Inc/color.h
+++ Programs/BunteBildah/Inc/color.h
@@ -2,10 +2,22 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#include "LCD_GUI.h"
+#include "LCD_general.h"
 
+/**
+  * @brief  reads the next 3 bytes of the file and returns them to the native
+  *         16 bit format used by the ITS-Board display. see RGBTRIPLE
+  *         of BMP_types.h and COLOR of LCD_general.h for more information.
+  * @retval the matching display color
+  */
 COLOR read_rgbtriple_as_color();
 
+/**
+  * @brief  reads the next 4 bytes of the file and returns them to the native
+  *         16 bit format used by the ITS-Board display. see RGBQUAD 
+  *         of BMP_types.h and COLOR of LCD_general.h for more information.
+  * @retval the matching display color
+  */
 COLOR read_rgbquad_as_color();
 
 #endif
--- Programs/BunteBildah/Src/color.c
+++ Programs/BunteBildah/Src/color.c
@@ -3,8 +3,8 @@
 #include "BMP_types.h"
 #include "input.h"
 
-#define UINT5_MAX 32
-#define UINT6_MAX 64
+#define UINT5_MAX  32
+#define UINT6_MAX  64
 #define UINT8_MAX 256
 
 static RGBTRIPLE triple;
@@ -15,13 +15,14 @@ static RGBQUAD quad;
 // =================
 
 /**
-  * @brief  helper function that takes actual 8-bit rgb values and
-  *         converts them to a 16-bit color
+  * @brief  helper function that takes actual 8-bit r, g & b values and
+  *         converts them to a single 16-bit color that is used by
+  *         the ITS-Board display.
   * @param  the rgb values
   * @retval the matching display color
   */
-COLOR bmp_to_display_color(char rgbBlue, char rgbGreen, char rgbRed) {
-    COLOR color = BLACK; // BLACK is 0
+static COLOR rgb_to_display_color(char rgbBlue, char rgbGreen, char rgbRed) {
+    COLOR color = (COLOR) 0;
 
     // normalize 8bit values to values between 0.0 and 1.0
     double blue_float = (double) rgbBlue / UINT8_MAX;
@@ -29,6 +30,8 @@ COLOR bmp_to_display_color(char rgbBlue, char rgbGreen, char rgbRed) {
     double red_float = (double) rgbRed / UINT8_MAX;
 
     // multiply normalized values by max of target values
+    // note that green uses 6 bit and red and blue 5 bit
+    // in the encoding used by the ITS-Board display.
     int blue_int = (int) blue_float * UINT5_MAX;
     int green_int = (int) green_float * UINT6_MAX;
     int red_int = (int) red_float * UINT5_MAX;
@@ -41,43 +44,25 @@ COLOR bmp_to_display_color(char rgbBlue, char rgbGreen, char rgbRed) {
     return color;
 }
 
-/**
-  * @brief  convert 24-bit rgbtriple to 16-bits needed by display.
-  * @param  triple  the rgbtriple
-  * @retval the matching display color
-  */
-COLOR rgbtriple_to_display_color(RGBTRIPLE* triple) {
-    return bmp_to_display_color(
-        triple->rgbtBlue,
-        triple->rgbtGreen,
-        triple->rgbtRed
-    );
-}
-
-/**
-  * @brief  convert 8-bit rgbquad to 16-bits needed by display.
-  * @param  triple  the rgbquad
-  * @retval the matching display color
-  */
-COLOR rgbquad_to_display_color(RGBQUAD* quad) {
-    return bmp_to_display_color(
-        quad->rgbBlue,
-        quad->rgbGreen,
-        quad->rgbRed
-    );
-}
-
 // ================
 // PUBLIC FUNCTIONS
 // ================
 
 COLOR read_rgbtriple_as_color() {
     COMread((char *) &triple, sizeof(RGBTRIPLE), 1);
-    return rgbtriple_to_display_color(&triple);
+    return rgb_to_display_color(
+        triple.rgbtBlue,
+        triple.rgbtGreen,
+        triple.rgbtRed
+    );
 }
 
 COLOR read_rgbquad_as_color() {
     COMread((char *) &quad, sizeof(RGBQUAD), 1);
-    return rgbquad_to_display_color(&quad);
+    return rgb_to_display_color(
+        quad.rgbBlue,
+        quad.rgbGreen,
+        quad.rgbRed
+    );
 }
 
