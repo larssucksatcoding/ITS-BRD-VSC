a62e850ba758d943ce83e4fc429e9c902f220d6a
Noah Rajko <wif507@haw-hamburg.de>
2025-12-10 15:32:14 +0100

    started implementing our writer

--- Programs/BunteBildah/Inc/writer.h
+++ Programs/BunteBildah/Inc/writer.h
@@ -1,10 +1,47 @@
+/**
+* @file         writer.h
+* @author       Lars Müller-Stumpf, Noah Rajko
+* @date         Dec 2025
+* @brief        writes to the display, draws the picture
+*/
 
 #ifndef WRITER_H
 #define WRITER_H
+#include "LCD_general.h"
 
-void draw_pixel(int x, int y, int color);
-void draw_line();
+/**
+* @brief        -
+*
+* @param        -
+* 
+* @return       -
+*/
+static void draw_pixel(COLOR color);
 
-void draw_picture();
+/**
+* @brief        -
+*
+* @param        -
+* 
+* @return       -
+*/
+static void draw_line();
+
+/**
+* @brief        -
+*
+* @param        -
+* 
+* @return       -
+*/
+extern void draw_picture();
 
 #endif
+
+/**
+* @brief        -
+*
+* @param        -
+* 
+* @return       -
+*/
--- Programs/BunteBildah/Src/writer.c
+++ Programs/BunteBildah/Src/writer.c
@@ -1,2 +1,33 @@
+/**
+* @file         writer.c
+* @author       Lars Müller-Stumpf, Noah Rajko
+* @date         Dec 2025
+* @brief        writes to the display, draws the picture
+*/
 
 #include "writer.h"
+#include "LCD_general.h"
+#include "reader.h"
+
+static int position_x;
+static int position_y;
+
+static void draw_pixel(COLOR color){
+
+}
+
+static void draw_line(){
+    COLOR* line = get_next_Line(); // change this somehow!!! we do not want this twice!!!!!
+    for(; position_x< LCD_WIDTH; position_x++){
+        draw_pixel(line[position_x]);
+    }
+
+}
+
+extern void draw_picture(){
+    position_x = 0;
+    position_y = 0;
+    for(; position_y < LCD_HEIGHT; position_y++){
+        draw_line();
+    }
+}
