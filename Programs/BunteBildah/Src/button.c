7e5f0d5d4f5f0ebb053e220620e58f9d224648a8
larssucksatcoding <wlz382@haw-hamburg.de>
2025-12-14 11:52:59 +0100

    the button code now actually detects *drum roll* button presses!!!!111!

--- Programs/BunteBildah/Src/button.c
+++ Programs/BunteBildah/Src/button.c
@@ -1,13 +1,15 @@
 #include "button.h"
 #include "stm32f4xx_hal.h"
+#include <stdint.h>
 
 /**
 * @brief Returns true if any button is pressed.
 */
 bool button_pressed() {
-	return (~GPIOF->IDR) != 0;
+	uint32_t pressed_button_mask = ~(GPIOF->IDR) & 0xFF;
+	return pressed_button_mask != 0;
 }
 
 void wait_until_button_pressed() {
-	while (button_pressed()) {};
+	while (!button_pressed()) {};
 }
