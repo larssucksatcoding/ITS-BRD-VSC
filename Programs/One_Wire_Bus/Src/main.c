0fe0c7b931d7e3dbcadd2dbf95bdbcf007cc4dc6
Noah Rajko <wif507@haw-hamburg.de>
2026-01-05 17:57:06 +0100

    make main for task 1

--- Programs/One_Wire_Bus/Src/main.c
+++ Programs/One_Wire_Bus/Src/main.c
@@ -7,80 +7,24 @@
 
 
 #include "bit_talk.h"
+#include "display.h"
 #include "time.h"
 #include "manager.h"
+#include "error_handler.h"
 
 #define ONE_SEC 1000
 
 int main(void) {
 	init();
+    int error = EOK;
 
     while(true) {
-        open_drain();
-        wait(2*ONE_SEC);
-        set_high();                     // PD0 stays off
-        wait(2*ONE_SEC);
-        set_low();
-
-
-        push_pull();
-        wait(2*ONE_SEC);
-        set_high();                     // PD0 goes on (slaves high current)
-        wait(2*ONE_SEC);
-        set_low();
-
-        open_drain();
-        wait(2*ONE_SEC);
-        set_high_1();                   // PD1 stays off
-        wait(2*ONE_SEC);
-        set_low_1();
-
-
-        push_pull();
-        wait(2*ONE_SEC);
-        set_high_1();                   // PD1 goes on but slaves don't get no current
-        wait(2*ONE_SEC);
-        set_low_1();
-
-        open_drain();
-        wait(2*ONE_SEC);
-        set_high_1();                   // PD1 stays off
-        wait(2*ONE_SEC);
-        set_high();                     // PD0 and PD1 go on slaves get current (low)
-        wait(2*ONE_SEC);
-        set_low();                      // both off
-        wait(2*ONE_SEC);
-        set_low_1();
-
-
-        push_pull();
-        wait(2*ONE_SEC);
-        set_high_1();                   // PD1 on slave low (no?) current
-        wait(2*ONE_SEC);
-        set_high();                     // both on slave high current
-        wait(2*ONE_SEC);
-        set_low();
-        wait(2*ONE_SEC);
-        set_low_1();
+        error = detect_slaves();
+        handle_the_hand_the_error_EXCLAMATION_MARK_NOW(error);
+        if (error == EOK){
+            write_rom();
+        }
+        wait(ONE_SEC);
     }
 
 }
-
-/*  SUMMARY
-Open-Drain:     the Bus is high, if PD1 and PD0 are high.
-*               one goes low - Bus goes low/zero
-
-Push-Pull:      the Bus is very high, if PD0 is on. 
-*               PD1 does not seem to make any difference 
-*               BEWARE: task says we're supposed to leave PD1 on in Push-Pull Mode
-*               (Reason for that is that else the current will be to high,
-*               since the resistor can be avoided)
-*/
-
-/**
-* @brief    -
-*
-* @param    -
-* 
-* @return   -
-*/
