4e67aceeb36ff4ddf87c84a754490305c0d7e72e
larssucksatcoding <wlz382@haw-hamburg.de>
2026-01-07 14:25:22 +0100

    makesmol: error + smol refactor

--- Programs/BunteBildah/Src/makesmoll.c
+++ Programs/BunteBildah/Src/makesmoll.c
@@ -14,17 +14,18 @@
 #include "reader.h"
 #include <math.h>
 #include <stdbool.h>
-#include "line.h"
+#include "errorhandler.h"
 
+
+// comment out this define to disable image compression
 #define MAKESMOL_C_USE_COMPRESSION
+
+
 #define RED_INDEX   0
 #define GREEN_INDEX 1
 #define BLUE_INDEX  2
 
 static int compression_ratio = 1;
-
-static COLOR compression_line[MAX_WIDTH];
-
 static int rgb_line[LCD_WIDTH*3];
 
 
@@ -35,6 +36,10 @@ static void reset_rgb_line() {
 }
 
 bool make_smoll() {
+    #ifndef MAKESMOL_C_USE_COMPRESSION
+    return false;
+
+    #else
     int pic_width = get_width();
     int pic_height = get_height();
 
@@ -42,32 +47,27 @@ bool make_smoll() {
     int factor_height =  ceil((double) pic_height / (double) LCD_HEIGHT);
 
     compression_ratio = (factor_width >= factor_height) ? factor_width : factor_height; 
-    if (compression_ratio > MAX_COMPRESSION_RATIO) {
-        // throw a temper tantrum.
-    }
 
-    #ifndef MAKESMOL_C_USE_COMPRESSION
-    return false;
-
-    #else
+    // throw a temper tantrum.
+    bool compressionRatioExceeded = compression_ratio > MAX_COMPRESSION_RATIO;
+    ERR_HANDLER(compressionRatioExceeded, "ich empfehle eine wilseco d 366 dampfwalze");
+    
     bool fits_on_display = ((pic_width) <= LCD_WIDTH) && ((pic_height) <= LCD_HEIGHT);
     return !fits_on_display;
     #endif
 }
 
 COLOR* get_compressed_line(COLOR* line) {
-    
-    // how many pixels are compressed to one
-    double pixel_amount = pow(compression_ratio, 2.0);
+    reset_rgb_line();
+
+    double pixel_amount = pow(compression_ratio, 2.0); // how many pixels are compressed to one
+    int compressed_picture_width = get_width() / compression_ratio;
 
     int line_red, line_green, line_blue;
     BYTE pixel_red, pixel_green, pixel_blue;
 
-    int compressed_picture_width = get_width() / compression_ratio;
-
-    reset_rgb_line();
-
     for (int y = 0; y < compression_ratio; y++) {
+        
         
         get_next_Line(line);
         
@@ -101,6 +101,7 @@ COLOR* get_compressed_line(COLOR* line) {
         COLOR pixel_color =  rgb_to_color(pixel_red, pixel_green, pixel_blue);
         line[i] = pixel_color;
     }
+
     for (int i = compressed_picture_width; i < LCD_WIDTH; i++) {
         line[i] = LCD_BACKGROUND;
     }
