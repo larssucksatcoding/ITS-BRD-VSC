3701bff7f1ce94359eee83e0d2dc029cfd051767
larssucksatcoding <wlz382@haw-hamburg.de>
2026-01-19 12:32:24 +0100

    clean up interrupt setup

--- Programs/Interruptz/Src/gpio.c
+++ Programs/Interruptz/Src/gpio.c
@@ -8,7 +8,9 @@
 #include "gpio.h"
 #include "encoder.h"
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include "stm32f429xx.h"
 #include "stm32f4xx.h"
 #include "math.h"
 
@@ -32,6 +34,15 @@
 #define ENCODER_A_INPUT_MASK    0b00000010     // Maske für den Input 
 #define ENCODER_B_INPUT_MASK    0b00000001
 
+/*  Interrupt  ---------------------------------------*/
+
+#define PORT_RESET      0x00
+#define PORT_G          0x06
+
+#define EXTI_BIT_WIDTH  4
+#define PIN_1           (EXTI_BIT_WIDTH * 1)
+#define PIN_2           (EXTI_BIT_WIDTH * 2)
+
 static int current_status;
 static int current_phase_count;
 static int input;
@@ -40,6 +51,66 @@ bool a_on_previous;
 bool b_on_previous;
 bool a_on;
 bool b_on;
+
+unsigned long get_clock_enable_mask(uint16_t port) {
+    /* definitions for clock enables:
+    #define RCC_AHB1ENR_GPIOAEN_Pos     (0U)                                
+    #define RCC_AHB1ENR_GPIOAEN_Msk     (0x1UL << RCC_AHB1ENR_GPIOAEN_Pos)
+    #define RCC_AHB1ENR_GPIOAEN         RCC_AHB1ENR_GPIOAEN_Msk 
+    so its just 0x1UL shifted to the right by the port index. */
+    return 0x1UL << port;
+}
+
+uint32_t get_syscfg_exticr_register(uint16_t pin) {
+    if (pin >  4) { return SYSCFG->EXTICR[0]; }
+    if (pin >  8) { return SYSCFG->EXTICR[1]; }
+    if (pin > 12) { return SYSCFG->EXTICR[2]; }
+    return SYSCFG->EXTICR[3];
+}
+
+void enable_interrupt_clocks(uint16_t port) {
+    unsigned long clock_enable_mask = get_clock_enable_mask(port);
+    RCC->AHB1ENR |= clock_enable_mask;      // clock for port
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;   // enable system conf. clock
+}
+
+void route_interrupt_pins(uint16_t pin, uint16_t port) {
+    uint32_t syscfg_exti_register = get_syscfg_exticr_register(pin);
+	syscfg_exti_register &= 0x00 << pin;	// remove old selection
+	syscfg_exti_register |= port << pin; 	// select port
+}
+
+void enable_interrupt_trigger(uint16_t pin, bool rising, bool falling) {
+    if (rising)  { EXTI->RTSR |= (1 << pin); }
+    if (falling) { EXTI->FTSR |= (1 << pin); }
+}
+
+void unmask_interrupt_pin(uint16_t pin) {
+    EXTI->IMR  |= (1 << pin);
+}
+
+void enable_interrupt(uint16_t pin, uint16_t port, uint32_t priority) {
+    int exti_irqn = EXTI0_IRQn + pin;
+
+    // interrupt enable works differently for pins 5-15
+    // EXTI4_IRQn is for pin 5
+    if (exti_irqn > EXTI4_IRQn) {
+        // have not looked into how to change this
+    }
+
+    NVIC_SetPriority(exti_irqn, priority);  // set priority
+    NVIC_EnableIRQ(exti_irqn);              // enable interrupt
+}
+
+void set_up_interrupt(uint16_t pin, uint16_t port, uint32_t priority, 
+    bool rising_trigger, bool falling_trigger) 
+{
+    enable_interrupt_clocks(port);
+    route_interrupt_pins(pin, port);
+    enable_interrupt_trigger(pin, rising_trigger, falling_trigger);
+    unmask_interrupt_pin(pin);
+    enable_interrupt(pin, port, priority);
+}
 
 void init_gpio(){
     // set GPIO registers to zero 
@@ -56,27 +127,8 @@ void init_gpio(){
     b_on = false;
 
     // configure interrupts
-
-    // TODO: clean this up with sensible defines?
-    // i just copied this from the lecture slides, don't really understand whats happening.
-    // might be wrong.
-
-    // Enable clocks
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;	// Clock for GPIO Port G
-	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; 	// System conf. Clock enable
-
-    // Route Pin 1 of Port G -> EXTI2
-	SYSCFG->EXTICR[0] &= (0x0f << (4 * 1));	// Remove old selection
-	SYSCFG->EXTICR[0] |=  0x06 << (4 * 1); 	// Select Port G
-    
-    // Route Pin 2 of Port G -> EXTI2
-	SYSCFG->EXTICR[0] &= (0x0f << (4 * 2));	// Remove old selection
-	SYSCFG->EXTICR[0] |=  0x06 << (4 * 2); 	// Select Port G
-
-    // Configure Pin 1
-    EXTI->RTSR |= (1 << 2); // select rising trigger for INT2
-    EXTI->FTSR |= (1 << 2); // select falling trigger for INT2
-    EXTI->IMR  |= (1 << 2); // unmask INT2
+    set_up_interrupt(1, 0x06, 0, true, true);
+    set_up_interrupt(2, 0x06, 0, true, true);
 }
 
 bool is_reset_button_pressed(){
